9501dae7d0e82f51d8a85425dee56b6fbccfec53
Rob Langley <rob.langley@fil.com>
2018-10-22 14:10:17 +0100

    merge changes since fork was taken

--- src/acceptance/app/app_suite_test.go
+++ src/acceptance/app/app_suite_test.go
@@ -1,10 +1,15 @@
 package app
 
 import (
+	"bytes"
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"math"
+	"net"
+	"net/http"
 	"strconv"
+	"strings"
 	"testing"
 	"time"
 
@@ -12,6 +17,7 @@ import (
 	. "acceptance/helpers"
 
 	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
+	"github.com/cloudfoundry-incubator/cf-test-helpers/generator"
 	"github.com/cloudfoundry-incubator/cf-test-helpers/helpers"
 	"github.com/cloudfoundry-incubator/cf-test-helpers/workflowhelpers"
 	. "github.com/onsi/ginkgo"
@@ -19,64 +25,18 @@ import (
 	. "github.com/onsi/gomega/gexec"
 )
 
-type Days string
-
 const (
-	daysOfMonth Days = "days_of_month"
-	daysOfWeek       = "days_of_week"
+	PolicyPath = "/v1/apps/{appId}/policy"
 )
-
-type appSummary struct {
-	RunningInstances int `json:"running_instances"`
-}
-
-type ScalingPolicy struct {
-	InstanceMin  int               `json:"instance_min_count"`
-	InstanceMax  int               `json:"instance_max_count"`
-	ScalingRules []*ScalingRule    `json:"scaling_rules,omitempty"`
-	Schedules    *ScalingSchedules `json:"schedules,omitempty"`
-}
-
-type ScalingRule struct {
-	MetricType            string `json:"metric_type"`
-	StatWindowSeconds     int    `json:"stat_window_secs"`
-	BreachDurationSeconds int    `json:"breach_duration_secs"`
-	Threshold             int64  `json:"threshold"`
-	Operator              string `json:"operator"`
-	CoolDownSeconds       int    `json:"cool_down_secs"`
-	Adjustment            string `json:"adjustment"`
-}
-
-type ScalingSchedules struct {
-	Timezone              string                  `json:"timezone,omitempty"`
-	RecurringSchedules    []*RecurringSchedule    `json:"recurring_schedule,omitempty"`
-	SpecificDateSchedules []*SpecificDateSchedule `json:"specific_date,omitempty"`
-}
-
-type RecurringSchedule struct {
-	StartTime             string `json:"start_time"`
-	EndTime               string `json:"end_time"`
-	DaysOfWeek            []int  `json:"days_of_week,omitempty"`
-	DaysOfMonth           []int  `json:"days_of_month,omitempty"`
-	ScheduledInstanceMin  int    `json:"instance_min_count"`
-	ScheduledInstanceMax  int    `json:"instance_max_count"`
-	ScheduledInstanceInit int    `json:"initial_min_instance_count"`
-}
-
-type SpecificDateSchedule struct {
-	StartDateTime         string `json:"start_date_time"`
-	EndDateTime           string `json:"end_date_time"`
-	ScheduledInstanceMin  int    `json:"instance_min_count"`
-	ScheduledInstanceMax  int    `json:"instance_max_count"`
-	ScheduledInstanceInit int    `json:"initial_min_instance_count"`
-}
-
-const MB = 1024 * 1024
 
 var (
 	cfg      *config.Config
 	setup    *workflowhelpers.ReproducibleTestSuiteSetup
 	interval int
+	client   *http.Client
+
+	instanceName         string
+	initialInstanceCount int
 )
 
 func TestAcceptance(t *testing.T) {
@@ -100,22 +60,41 @@ var _ = BeforeSuite(func() {
 	setup.Setup()
 
 	workflowhelpers.AsUser(setup.AdminUserContext(), cfg.DefaultTimeoutDuration(), func() {
-		EnableServiceAccess(cfg, setup.GetOrganizationName())
+		if cfg.IsServiceOfferingEnabled() {
+			EnableServiceAccess(cfg, setup.GetOrganizationName())
+		}
 	})
-
-	serviceExists := cf.Cf("marketplace", "-s", cfg.ServiceName).Wait(cfg.DefaultTimeoutDuration())
-	Expect(serviceExists).To(Exit(0), fmt.Sprintf("Service offering, %s, does not exist", cfg.ServiceName))
+	if cfg.IsServiceOfferingEnabled() {
+		serviceExists := cf.Cf("marketplace", "-s", cfg.ServiceName).Wait(cfg.DefaultTimeoutDuration())
+		Expect(serviceExists).To(Exit(0), fmt.Sprintf("Service offering, %s, does not exist", cfg.ServiceName))
+	}
 
 	interval = cfg.AggregateInterval
-	if interval < 60 {
-		interval = 60
+
+	client = &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+			Dial: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).Dial,
+			TLSHandshakeTimeout: 10 * time.Second,
+			DisableCompression:  true,
+			DisableKeepAlives:   true,
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: cfg.SkipSSLValidation,
+			},
+		},
+		Timeout: 30 * time.Second,
 	}
 
 })
 
 var _ = AfterSuite(func() {
 	workflowhelpers.AsUser(setup.AdminUserContext(), cfg.DefaultTimeoutDuration(), func() {
-		DisableServiceAccess(cfg, setup.GetOrganizationName())
+		if cfg.IsServiceOfferingEnabled() {
+			DisableServiceAccess(cfg, setup.GetOrganizationName())
+		}
 	})
 	setup.Teardown()
 })
@@ -216,138 +195,6 @@ func averageStatsUsedByInstance(appGUID string, timeout time.Duration) (uint64, 
 	return avgMem, avgCpu
 }
 
-func generateDynamicScaleOutPolicy(instanceMin, instanceMax int, metricName string, threshold int64) string {
-	scalingOutRule := ScalingRule{
-		MetricType:            metricName,
-		StatWindowSeconds:     interval,
-		BreachDurationSeconds: interval,
-		Threshold:             threshold,
-		Operator:              ">=",
-		CoolDownSeconds:       interval,
-		Adjustment:            "+1",
-	}
-
-	policy := ScalingPolicy{
-		InstanceMin:  instanceMin,
-		InstanceMax:  instanceMax,
-		ScalingRules: []*ScalingRule{&scalingOutRule},
-	}
-	bytes, err := json.Marshal(policy)
-	Expect(err).NotTo(HaveOccurred())
-
-	return string(bytes)
-}
-
-func generateDynamicScaleInPolicy(instanceMin, instanceMax int, metricName string, threshold int64) string {
-	scalingInRule := ScalingRule{
-		MetricType:            metricName,
-		StatWindowSeconds:     interval,
-		BreachDurationSeconds: interval,
-		Threshold:             threshold,
-		Operator:              "<",
-		CoolDownSeconds:       interval,
-		Adjustment:            "-1",
-	}
-
-	policy := ScalingPolicy{
-		InstanceMin:  instanceMin,
-		InstanceMax:  instanceMax,
-		ScalingRules: []*ScalingRule{&scalingInRule},
-	}
-	bytes, err := json.Marshal(policy)
-	Expect(err).NotTo(HaveOccurred())
-
-	return string(bytes)
-}
-
-func generateDynamicAndSpecificDateSchedulePolicy(instanceMin, instanceMax int, threshold int64,
-	timezone string, startDateTime, endDateTime time.Time,
-	scheduledInstanceMin, scheduledInstanceMax, scheduledInstanceInit int) string {
-
-	scalingInRule := ScalingRule{
-		MetricType:            "memoryused",
-		StatWindowSeconds:     interval,
-		BreachDurationSeconds: interval,
-		Threshold:             threshold,
-		Operator:              "<",
-		CoolDownSeconds:       interval,
-		Adjustment:            "-1",
-	}
-
-	specificDateSchedule := SpecificDateSchedule{
-		StartDateTime:         startDateTime.Format("2006-01-02T15:04"),
-		EndDateTime:           endDateTime.Format("2006-01-02T15:04"),
-		ScheduledInstanceMin:  scheduledInstanceMin,
-		ScheduledInstanceMax:  scheduledInstanceMax,
-		ScheduledInstanceInit: scheduledInstanceInit,
-	}
-
-	policy := ScalingPolicy{
-		InstanceMin:  instanceMin,
-		InstanceMax:  instanceMax,
-		ScalingRules: []*ScalingRule{&scalingInRule},
-		Schedules: &ScalingSchedules{
-			Timezone:              timezone,
-			SpecificDateSchedules: []*SpecificDateSchedule{&specificDateSchedule},
-		},
-	}
-
-	bytes, err := json.Marshal(policy)
-	Expect(err).NotTo(HaveOccurred())
-
-	return string(bytes)
-}
-
-func generateDynamicAndRecurringSchedulePolicy(instanceMin, instanceMax int, threshold int64,
-	timezone string, startTime, endTime time.Time, daysOfMonthOrWeek Days,
-	scheduledInstanceMin, scheduledInstanceMax, scheduledInstanceInit int) string {
-
-	scalingInRule := ScalingRule{
-		MetricType:            "memoryused",
-		StatWindowSeconds:     interval,
-		BreachDurationSeconds: interval,
-		Threshold:             threshold,
-		Operator:              "<",
-		CoolDownSeconds:       interval,
-		Adjustment:            "-1",
-	}
-
-	recurringSchedule := RecurringSchedule{
-		StartTime:             startTime.Format("15:04"),
-		EndTime:               endTime.Format("15:04"),
-		ScheduledInstanceMin:  scheduledInstanceMin,
-		ScheduledInstanceMax:  scheduledInstanceMax,
-		ScheduledInstanceInit: scheduledInstanceInit,
-	}
-
-	if daysOfMonthOrWeek == daysOfMonth {
-		day := startTime.Day()
-		recurringSchedule.DaysOfMonth = []int{day}
-
-	} else {
-		day := int(startTime.Weekday())
-		if day == 0 {
-			day = 7
-		}
-		recurringSchedule.DaysOfWeek = []int{day}
-	}
-
-	policy := ScalingPolicy{
-		InstanceMin:  instanceMin,
-		InstanceMax:  instanceMax,
-		ScalingRules: []*ScalingRule{&scalingInRule},
-		Schedules: &ScalingSchedules{
-			Timezone:           timezone,
-			RecurringSchedules: []*RecurringSchedule{&recurringSchedule},
-		},
-	}
-
-	bytes, err := json.Marshal(policy)
-	Expect(err).NotTo(HaveOccurred())
-
-	return string(bytes)
-}
-
 func getStartAndEndTime(location *time.Location, offset, duration time.Duration) (time.Time, time.Time) {
 	// Since the validation of time could fail if spread over two days and will result in acceptance test failure
 	// Need to fix dates in that case.
@@ -357,4 +204,62 @@ func getStartAndEndTime(location *time.Location, offset, duration time.Duration)
 	}
 	endTime := startTime.Add(duration)
 	return startTime, endTime
+}
+
+func doAPIRequest(req *http.Request) (*http.Response, error) {
+	resp, err := client.Do(req)
+	return resp, err
+}
+
+func CreatePolicyWithAPI(appGUID, policy string) {
+	oauthToken := OauthToken(cfg)
+	policyURL := fmt.Sprintf("%s%s", cfg.ASApiEndpoint, strings.Replace(PolicyPath, "{appId}", appGUID, -1))
+	req, err := http.NewRequest("PUT", policyURL, bytes.NewBuffer([]byte(policy)))
+	req.Header.Add("Authorization", oauthToken)
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := doAPIRequest(req)
+	Expect(err).ShouldNot(HaveOccurred())
+	defer resp.Body.Close()
+	Expect(resp.StatusCode == 200 || resp.StatusCode == 201).Should(BeTrue())
+
+}
+
+func DeletePolicyWithAPI(appGUID string) {
+	oauthToken := OauthToken(cfg)
+	policyURL := fmt.Sprintf("%s%s", cfg.ASApiEndpoint, strings.Replace(PolicyPath, "{appId}", appGUID, -1))
+	req, err := http.NewRequest("DELETE", policyURL, nil)
+	req.Header.Add("Authorization", oauthToken)
+
+	resp, err := doAPIRequest(req)
+	Expect(err).ShouldNot(HaveOccurred())
+	defer resp.Body.Close()
+	Expect(resp.StatusCode == 200).Should(BeTrue())
+
+}
+
+func CreatePolicy(appName, appGUID, policy string) {
+	if cfg.IsServiceOfferingEnabled() {
+		instanceName = generator.PrefixedRandomName("autoscaler", "service")
+		createService := cf.Cf("create-service", cfg.ServiceName, cfg.ServicePlan, instanceName).Wait(cfg.DefaultTimeoutDuration())
+		Expect(createService).To(Exit(0), "failed creating service")
+
+		bindService := cf.Cf("bind-service", appName, instanceName, "-c", policy).Wait(cfg.DefaultTimeoutDuration())
+		Expect(bindService).To(Exit(0), "failed binding service to app with a policy ")
+	} else {
+		CreatePolicyWithAPI(appGUID, policy)
+	}
+
+}
+
+func DeletePolicy(appName, appGUID string) {
+	if cfg.IsServiceOfferingEnabled() {
+		unbindService := cf.Cf("unbind-service", appName, instanceName).Wait(cfg.DefaultTimeoutDuration())
+		Expect(unbindService).To(Exit(0), "failed unbinding service from app")
+		deleteService := cf.Cf("delete-service", instanceName, "-f").Wait(cfg.DefaultTimeoutDuration())
+		Expect(deleteService).To(Exit(0))
+	} else {
+		DeletePolicyWithAPI(appGUID)
+	}
+
 }
